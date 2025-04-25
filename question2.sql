ac20c9af70ff42c922c05082150256775302b8be
Caprice-Instinct <linetwangui04@gmail.com>
2025-04-25 11:46:23 +0300

    modified question 2 sql

--- question2.sql
+++ question2.sql
@@ -1,3 +1,7 @@
+CREATE DATABASE contact_book;
+
+USE contact_book;
+
 CREATE TABLE users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
@@ -12,3 +16,21 @@ CREATE TABLE contacts (
     email VARCHAR(100),
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
+
+INSERT INTO users (username, email) VALUES
+('sharon_ken', 'sharon.ken@example.com'),
+('brian_otieno', 'brian.otieno@example.com'),
+('lucy_wanjiku', 'lucy.wanjiku@example.com');
+
+INSERT INTO contacts (user_id, name, phone, email) VALUES
+(1, 'Jane Muthoni', '0712345678', 'jane.muthoni@gmail.com'),
+(1, 'Peter Mwangi', '0722123456', 'peter.mwangi@yahoo.com'),
+(2, 'Mercy Achieng', '0733111222', 'mercy.achieng@outlook.com'),
+(2, 'Tom Njoroge', '0700778899', 'tom.njoroge@gmail.com'),
+(3, 'Linda Chebet', '0744332211', 'linda.chebet@icloud.com');
+
+
+
+SELECT * FROM users;
+SELECT * FROM contacts;
+
